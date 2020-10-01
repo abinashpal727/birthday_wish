7af2f04dd4eca8aba77071a69c2529ab210de143
abinashpal727 <abinashpal727@gmail.com>
2020-10-01 10:23:27 +0530

    initiated

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "ANWESHA SAHA"&& b=="I LOVE YOU")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
